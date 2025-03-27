c3c178df299fc20e5026e6981f4f1d6a3dbb65be
Roman <derromaann@gmail.com>
2025-03-27 06:49:01 +0100

    update of my console programm

--- js/script.js
+++ js/script.js
@@ -1,35 +1,39 @@
 'use strict';
-// const numberOfFilms = +prompt('How many films have you watched?"', '');
+const numberOfFilms = +prompt('How many films have you watched?"', '');
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
-
-// let a = prompt('Tell us One of the last watched movies', ''),
-//     b = prompt('Give us ur assesment', ''),
-//     c = prompt('Tell us One of the last watched movies', ''),
-//     d = prompt('Give us ur assesment', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
 
-let result = '';
-let length = 7;
+for (let i = 0; i < 2; i++) {
 
-for (let i = 0; i < length; i++) {
+    let a = prompt('Tell us One of the last watched movies', ''),
+        b = +prompt('Give us ur assesment', '');
 
-    for (let j = 0; j < i; j++) {
-        result += '*';
+    if (a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Success!')
+    } else {
+        alert('error. Write it down correctly');
+        i--;
     }
+};
 
-    result += '\n';
-}
+if (personalMovieDB.count < 10) {
+    alert('Переглянуто достатньо мало фільмів!')
+} else if (personalMovieDB.count >= 10 || personalMovieDB.count <= 30) {
+    alert('Ви класичний глядач')
+} else if (personalMovieDB.count > 30) {
+    alert('Ви кіноман!')
+} else {
+    alert('Відбулася помилка')
+};
 
-console.log(result);
+
+
+console.log(personalMovieDB);
