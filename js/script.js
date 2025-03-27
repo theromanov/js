30f2867eb14404c1d20acdb5a10d2c1828326df2
Roman <derromaann@gmail.com>
2025-03-27 04:27:29 +0100

    created a 6-star task using cycle

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
-const numberOfFilms = +prompt('How many films have you watched?"', '');
+'use strict';
+// const numberOfFilms = +prompt('How many films have you watched?"', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-let a = prompt('Tell us One of the last watched movies', ''),
-    b = prompt('Give us ur assesment', ''),
-    c = prompt('Tell us One of the last watched movies', ''),
-    d = prompt('Give us ur assesment', '');
+// let a = prompt('Tell us One of the last watched movies', ''),
+//     b = prompt('Give us ur assesment', ''),
+//     c = prompt('Tell us One of the last watched movies', ''),
+//     d = prompt('Give us ur assesment', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
+let result = '';
+let length = 7;
+
+for (let i = 0; i < length; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+
+    result += '\n';
+}
+
+console.log(result);
